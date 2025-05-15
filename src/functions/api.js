d89d3713b0eec775da3129cad136133048f2f235
Tirth-2504 <tirthpatel2130@gmail.com>
2025-05-14 18:56:42 -0600

    Update

---
+++ src/functions/api.js
@@ -0,0 +1,13 @@
+async function fetchData(url) {
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error(`There was a networking error`);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+module.exports = fetchData;
