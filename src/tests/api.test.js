e4807223df9897ffe3aca60496f6ee42c5610ca2
Tirth-2504 <tirthpatel2130@gmail.com>
2025-05-20 18:50:28 -0600

    Update

--- src/tests/api.test.js
+++ src/tests/api.test.js
@@ -1,20 +1,34 @@
-const { compile } = require('ejs');
 const fetchData = require('../functions/api.js');
 
-desribe('fetchData', () => {
-    //mock this globle fetch function
+ //mock this globle fetch function
     global.fetch = jest.fn();
 
+describe('fetchData', () => {
+   beforeEach(() => {
+        //reset the mock object before each test
+        fetch.mockClear();
+    });
+
     
-    it('fetchData dataa from an API endpoint and return it as JSON', () => {
+    it('fetches data from an API endpoint and returns it as JSON', async () => {
+
+        //AAA  pattern
+
+        //Assemble
         //mock the function response
-        const mockResponse = {userId: 1, id: 1, title: 'delectus aut autem', compile: false};
+        const mockResponse = {userId: 1, id: 1, title: 'delectus aut autem', "compile": false};
 
         fetch.mockResolvedValueOnce({
             ok: true,
             json: jest.fn().mockResolvedValueOnce(mockResponse),
         });
 
+        //Action
+        const data = await fetchData('https://jsonplaceholder.typicode.com/todos/1');
+
+        //Assert
+        expect(data).toEqual(mockResponse);
+        expect(fetch).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/todos/1');
     });
 
 });
