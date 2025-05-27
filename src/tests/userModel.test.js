551ecec5c19e686ce854c768275612bc83af89c7
Tirth-2504 <tirthpatel2130@gmail.com>
2025-05-26 19:03:15 -0600

    Update

---
+++ src/tests/userModel.test.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { User, createUser } = require('../functions/userModel');
+
+//test suite
+describe('User Model Tests', () => {
+    //jest hook
+    beforeEach( () => {
+        jest.clearAllMocks;
+    });
+
+    it('should create a new user', async () => {
+
+    });
+
+    
+});
+        
