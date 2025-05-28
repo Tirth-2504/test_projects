a53f88a255222b358e955d11638d9d1d26f4676d
Tirth-2504 <tirthpatel2130@gmail.com>
2025-05-27 18:33:10 -0600

    Update

--- src/tests/userModel.test.js
+++ src/tests/userModel.test.js
@@ -9,9 +9,21 @@ describe('User Model Tests', () => {
     });
 
     it('should create a new user', async () => {
+        //Arrange
+        const mockUser = {
+            firstName: 'Tirth Patel',
+            email: 'tirthpatel7411.com',
+            password: '01110100 01100101 01100011 01101000',
+            age: 21,
+        }
+         
+        //Action
+        const result1 = jest.spyOn(User.prototype, 'save').mockImplementation(mockUser);
+        const result2 = new createUser('Tirth Patel', 'tirthpatel7411.com', '01110100 01100101 01100011 01101000', 21);
 
-    });
-
-    
+        //Assert
+        expect(result).toEqual(result2);
+        expect(User.prototype.save()).toHaveBeenCalledTimes(1);
+    });  
 });
-        
+         
